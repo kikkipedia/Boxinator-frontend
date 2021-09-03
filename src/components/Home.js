ca43412b5722a1d4bfdaab3daf82a9f7dee33cde
kikkipedia <kristinakerlund@gmail.com>
2021-09-03 14:30:39 +0200

    Log out from keycloak

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,15 @@
-
+import { useHistory } from "react-router-dom"
+import Keycloak from "keycloak-js"
 
 const Home = () => {
 
+    const logOut = () => {
+        let redirectURI = "http://localhost:3000"
+        window.location.href="https://keycloak-boxinator2.herokuapp.com/auth/realms/boxinator-app/protocol/openid-connect/logout?redirect_uri=" + redirectURI
+    }
+
     return(
-        <button>LOG OUT</button>
+        <button onClick={logOut}>LOG OUT</button>
     )
 }
 export default Home
