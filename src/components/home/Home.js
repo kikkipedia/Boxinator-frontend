20ad7488ed021b76ee0556ac4b317e47572f9a61
danielDumville <daniel.dumville@se.experis.com>
2021-09-10 13:06:02 +0200

    Commented out extra text

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -15,10 +15,10 @@ const Home = () => {
       <h1>Home Page</h1>
        
       <strong>Anyone can access this page</strong>
-            {initialized ?
+            {/* {initialized ?
         keycloak.authenticated && <pre >{JSON.stringify(keycloak, undefined, 2)}</pre>
         : <h2>keycloak initializing ....!!!!</h2>
-      }
+      } */}
     </div>
   )
 }
